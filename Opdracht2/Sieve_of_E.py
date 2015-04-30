d892cd3b5db076aea26e4b739d025f426a9b687e
Niels Schaminee <nielsschaminee@gmail.com>
2015-04-30 11:32:53 +0000

    getting python to give the correct output for a dummy list

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,14 +1,16 @@
 import sys
 import time
 
-# sys.argv is a list with the command-line arguments. sysv.arg[0] is the name of Python script
 T1 = time.perf_counter()
 
 maxnumber = int(sys.argv[1])
 output = open(sys.argv[2], 'w')
 
-for i in range(maxnumber):
-    output.write(str(i) + '\n' )
+primlist = [2, 3, 5, 7]
+
+for i in range(len(primlist)):
+    output.write(str(primlist[i]) + '\n')
+
 
 T2 = time.perf_counter()
 
